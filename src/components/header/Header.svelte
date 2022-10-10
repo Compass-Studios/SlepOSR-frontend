ab2128d769a2f2436a62bfff1eadb995395aa6ca
S4vyss <mlodywojownik345@gmail.com>
2022-10-10 15:07:20 +0200

    Starting header

--- src/components/header/Header.svelte
+++ src/components/header/Header.svelte
@@ -4,10 +4,24 @@
 
 <header>
     <article class="navbar">
-        siema
-    </article>
-    <article class="searchbar">
-        siema 3
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Sklep</a>
+                </li>
+                <li>
+                    <a href="#">O nas</a>
+                </li>
+                <li>
+                    <a href="#">Galeria</a>
+                </li>
+            </ul>
+            <div class="burger">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
+        </nav>
     </article>
 </header>
 
@@ -16,15 +30,49 @@
       display: flex;
       justify-content: space-between;
       align-items: center;
-      height: 100%;
+      height: 20%;
       width: 100%;
 
       .navbar {
         margin-left: 1em;
-      }
+        text-transform: uppercase;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
 
-      .searchbar {
-        margin-right: 1em;
+        nav {
+          ul {
+            display: flex;
+            flex-direction: column;
+            list-style: none;
+            height: 100%;
+
+            li {
+              font-size: 20px;
+              font-weight: bold;
+              letter-spacing: 2px;
+
+              a {
+                text-decoration: none;
+              }
+            }
+
+            @media screen and (max-width: 800px) {
+              display: none;
+            }
+          }
+          .burger {
+            display: none;
+
+            div {
+              width: 25px;
+              height: 2px;
+              background-color: white;
+              margin: 3px;
+            }
+          }
+        }
       }
     }
 </style>
--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -7,15 +7,64 @@
         <Header />
     </section>
     <section class="sklep">
-        <h1>
-            sklep
-        </h1>
+        <div class="logo">
+            <img src="src/assets/Zrzut_ekranu_2022-09-27_o_19.29-fococlipping-standard.png" />
+        </div>
     </section>
+    <article class="searchbar">
+        <nav>
+            <img src="src/assets/magnifying-glass-solid.svg" width="25" height="25" />
+        </nav>
+    </article>
 </main>
 
 <style type="text/scss">
-    .header {
+    main {
+      height: 100vh;
+      display: flex;
+    }
+
+    .sklep {
       width: 100%;
-      height: 5vh;
+      display: flex;
+      justify-content: center;
+
+      .logo {
+        height: 25%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        img {
+          transform: scale(.7);
+
+          @media screen and (max-width: 675px) {
+            transform: scale(.45);
+            width: 470px;
+            height: 300px;
+          }
+        }
+      }
+    }
+
+    .header {
+      width: auto;
+      min-height: 100%;
+    }
+
+    .searchbar {
+      width: 5%;
+      margin-right: 1em;
+
+      nav {
+        height: 15%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        img {
+          filter: invert(100%);
+        }
+      }
     }
 </style>
